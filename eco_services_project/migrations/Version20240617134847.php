a84938404d0e599dfb5407149948427f33a3a1d8
HylaDev <ernestdossa.9@gmail.com>
2024-06-18 13:54:14 +0200

    update checkout page design

---
+++ eco_services_project/migrations/Version20240617134847.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240617134847 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users ADD siren VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE users ALTER firstname DROP NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE users DROP siren');
+        $this->addSql('ALTER TABLE users ALTER firstname SET NOT NULL');
+    }
+}
